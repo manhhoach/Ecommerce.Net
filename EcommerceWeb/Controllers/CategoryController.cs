b2c4d2c909bf43cbbe64f9621e04a7381aa647ee
manhhoach <manhhn.nv5@gmail.com>
2024-03-02 16:10:23 +0700

    init prj

---
+++ EcommerceWeb/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using EcommerceWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _db;
+        public CategoryController(AppDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var data = _db.Categories.OrderBy(x => x.DisplayOrder).ToList();
+            return View(data);
+        }
+    }
+}
